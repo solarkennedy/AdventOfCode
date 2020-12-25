d13e43d632060e00562d520f623608bf7e171340
Kyle Anderson <kyle@xkyle.com>
2020-12-25 11:03:41 -0800

    2020 Day 4 part two

--- 2020/4/main.go
+++ 2020/4/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/solarkennedy/AdventOfCode/utils"
@@ -20,15 +22,15 @@ type passport struct {
 }
 
 func splitPassportInputs(input string) []string {
-	chunks := []string{}
-	for _, chunk := range strings.Split(input, "\n\n") {
-		chunks = append(chunks, chunk)
-	}
-	return chunks
+	return strings.Split(input, "\n\n")
 }
 
 func parseKV(kv string) (string, string) {
 	v := strings.Split(kv, ":")
+	if len(v) != 2 {
+		fmt.Printf("Couldn't parse the the : kv: '%s'", kv)
+		panic(v)
+	}
 	return v[0], v[1]
 }
 
@@ -52,13 +54,15 @@ func attributeKVToPassport(p *passport, k string, v string) {
 }
 
 func parsePassport(input string) passport {
-	fmt.Println(input)
 	p := passport{}
 	for _, line := range strings.Split(input, "\n") {
 		if line == "" {
 			continue
 		}
 		for _, kv := range strings.Split(line, " ") {
+			if kv == "" || kv == "\t" {
+				continue
+			}
 			k, v := parseKV(kv)
 			attributeKVToPassport(&p, k, v)
 		}
@@ -75,10 +79,10 @@ func parseBatchInput(input string) []passport {
 	return passports
 }
 
-// isValidPassport validates by verifying that all fields
+// isValidPassportPartOne validates by verifying that all fields
 // are set, *except* `cid`, which is OK to be unset
 // because we are dirty
-func isValidPassport(p passport) (bool, error) {
+func isValidPassportPartOne(p passport) (bool, error) {
 	rv := reflect.ValueOf(&p).Elem()
 	typeOfS := rv.Type()
 	for i := 0; i < rv.NumField(); i++ {
@@ -94,7 +98,7 @@ func isValidPassport(p passport) (bool, error) {
 func partOne(passports []passport) int {
 	counter := 0
 	for _, passport := range passports {
-		isValid, err := isValidPassport(passport)
+		isValid, err := isValidPassportPartOne(passport)
 		if isValid {
 			counter += 1
 		} else {
@@ -104,8 +108,145 @@ func partOne(passports []passport) int {
 	return counter
 }
 
+// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+func validateByr(p passport) (bool, error) {
+	y, err := strconv.Atoi(p.Byr)
+	if err != nil {
+		return false, fmt.Errorf("Failed to parse byr: %w", err)
+	}
+	if y < 1920 || y > 2002 {
+		return false, fmt.Errorf("byr '%d' out of range, must be between 1920 and 2002 inclusive", y)
+	}
+	return true, nil
+}
+
+// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+func validateIyr(p passport) (bool, error) {
+	y, err := strconv.Atoi(p.Iyr)
+	if err != nil {
+		return false, fmt.Errorf("Failed to parse Iyr: %w", err)
+	}
+	if y < 2010 || y > 2020 {
+		return false, fmt.Errorf("iyr '%d' out of range, must be between 2010 and 2020 inclusive", y)
+	}
+	return true, nil
+}
+
+// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+func validateEyr(p passport) (bool, error) {
+	y, err := strconv.Atoi(p.Eyr)
+	if err != nil {
+		return false, fmt.Errorf("Failed to parse Eyr: %w", err)
+	}
+	if y < 2020 || y > 2030 {
+		return false, fmt.Errorf("eyr '%d' out of range, must be between 2020 and 2030 inclusive", y)
+	}
+	return true, nil
+}
+
+// hgt (Height) - a number followed by either cm or in:
+// 	If cm, the number must be at least 150 and at most 193.
+// 	If in, the number must be at least 59 and at most 76.
+func validateHgt(p passport) (bool, error) {
+	r := regexp.MustCompile(`(?P<Value>\d+)(?P<Unit>in|cm)`)
+	//r := regexp.MustCompile(`(?P<Value>.*)`)
+	match := r.FindStringSubmatch(p.Hgt)
+	if len(match) != 3 {
+		return false, fmt.Errorf("Input hgt: '%s' didn't match our regex '%s'", p.Hgt, r.String())
+	}
+	unit := match[2]
+	value, err := strconv.Atoi(match[1])
+	if err != nil {
+		return false, fmt.Errorf("Could not parse value of height: '%s' for '%s'", err, p.Hgt)
+	}
+	if unit == "cm" {
+		if value < 150 || value > 193 {
+			return false, fmt.Errorf("Height must be between 150-193 cm: '%s'", p.Hgt)
+		}
+	} else if unit == "in" {
+		if value < 59 || value > 76 {
+			return false, fmt.Errorf("Height must be between 59-76 in: '%s'", p.Hgt)
+		}
+	} else {
+		return false, fmt.Errorf("Unknown hight unit '%s' on '%s'", unit, p.Hgt)
+	}
+	return true, nil
+}
+
+// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+func validateHcl(p passport) (bool, error) {
+	r := regexp.MustCompile(`#[0-9a-f]{6}`)
+	match := r.Match([]byte(p.Hcl))
+	if !match {
+		return false, fmt.Errorf("hcl '%s' didn't match our regex '%s'", p.Hcl, r.String())
+	}
+	return true, nil
+}
+
+// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+func validateEcl(p passport) (bool, error) {
+	r := regexp.MustCompile(`(amb|blu|brn|gry|grn|hzl|oth)`)
+	if !r.Match([]byte(p.Ecl)) {
+		return false, fmt.Errorf("ecl '%s' didn't match our regex '%s'", p.Ecl, r.String())
+	}
+	return true, nil
+}
+
+// pid (Passport ID) - a nine-digit number, including leading zeroes.
+func validatePid(p passport) (bool, error) {
+	r := regexp.MustCompile(`^\d{9}$`)
+	match := r.MatchString(p.Pid)
+	if !match {
+		return false, fmt.Errorf("pid '%s' didn't match our regex '%s'", p.Pid, r.String())
+	}
+	return true, nil
+}
+
+// cid (Country ID) - ignored, missing or not.
+func validateCid(p passport) (bool, error) {
+	return true, nil
+}
+
+// isValidPassportPartTwo does the stricter validation per the rules:
+func isValidPassportPartTwo(p passport) (bool, error) {
+	if ok, err := validateByr(p); !ok {
+		return false, err
+	}
+	if ok, err := validateIyr(p); !ok {
+		return false, err
+	}
+	if ok, err := validateEyr(p); !ok {
+		return false, err
+	}
+	if ok, err := validateHgt(p); !ok {
+		return false, err
+	}
+	if ok, err := validateHcl(p); !ok {
+		return false, err
+	}
+	if ok, err := validateEcl(p); !ok {
+		return false, err
+	}
+	if ok, err := validatePid(p); !ok {
+		return false, err
+	}
+	if ok, err := validateCid(p); !ok {
+		return false, err
+	}
+	return true, nil
+}
+
 func partTwo(passports []passport) int {
-	return 0
+	counter := 0
+	for _, passport := range passports {
+		isValid, err := isValidPassportPartTwo(passport)
+		if isValid {
+			counter += 1
+		} else {
+			fmt.Printf("Invalid passport: %s\n", err)
+		}
+	}
+	return counter
 }
 
 func main() {
--- 2020/4/main_test.go
+++ 2020/4/main_test.go
@@ -36,21 +36,21 @@ func TestAttributeKVToPassport(t *testing.T) {
 	assert.Equal(t, p.Eyr, "42")
 }
 
-func TestIsValidPassportGood(t *testing.T) {
+func TestIsValidPassportPartOneGood(t *testing.T) {
 	input := `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm`
 	p := parsePassport(input)
-	isValid, err := isValidPassport(p)
+	isValid, err := isValidPassportPartOne(p)
 	// "all eight fields are present"
 	assert.True(t, isValid)
 	assert.Nil(t, err)
 }
 
-func TestIsValidPassportBad(t *testing.T) {
+func TestIsValidPassportPartOneBad(t *testing.T) {
 	input := `iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
 hcl:#cfa07d byr:1929`
 	p := parsePassport(input)
-	isValid, err := isValidPassport(p)
+	isValid, err := isValidPassportPartOne(p)
 	// "it is missing hgt"
 	assert.False(t, isValid)
 	assert.NotNil(t, err)
@@ -60,4 +60,86 @@ func TestPartOne(t *testing.T) {
 	passports := parseBatchInput(testBatch)
 	actual := partOne(passports)
 	assert.Equal(t, 2, actual)
+}
+
+func TestPartTwoInvalidExamples(t *testing.T) {
+	invalidPassports := `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+`
+	passports := parseBatchInput(invalidPassports)
+	assert.Equal(t, 4, len(passports))
+	actual := partTwo(passports)
+	assert.Equal(t, 0, actual)
+}
+
+func TestPartTwoValidExamples(t *testing.T) {
+	validPassports := `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
+	passports := parseBatchInput(validPassports)
+	assert.Equal(t, 4, len(passports))
+	actual := partTwo(passports)
+	assert.Equal(t, 4, actual)
+}
+
+func TestPartTwoValidations(t *testing.T) {
+	var ok bool
+
+	ok, _ = validateByr(passport{Byr: "2000"})
+	assert.True(t, ok)
+	ok, _ = validateByr(passport{Byr: "1900"})
+	assert.False(t, ok)
+
+	ok, _ = validateIyr(passport{Iyr: "2019"})
+	assert.True(t, ok)
+	ok, _ = validateIyr(passport{Iyr: "1990"})
+	assert.False(t, ok)
+
+	ok, _ = validateEyr(passport{Eyr: "2022"})
+	assert.True(t, ok)
+	ok, _ = validateEyr(passport{Eyr: "1990"})
+	assert.False(t, ok)
+
+	ok, _ = validateHgt(passport{Hgt: "149cm"})
+	assert.False(t, ok)
+	ok, _ = validateHgt(passport{Hgt: "58in"})
+	assert.False(t, ok)
+	ok, _ = validateHgt(passport{Hgt: "foo"})
+	assert.False(t, ok)
+
+	ok, _ = validateHcl(passport{Hcl: "#ffffff"})
+	assert.True(t, ok)
+	ok, _ = validateHcl(passport{Hcl: "white"})
+	assert.False(t, ok)
+
+	ok, _ = validateEcl(passport{Ecl: "blu"})
+	assert.True(t, ok)
+	ok, _ = validateEcl(passport{Ecl: "white"})
+	assert.False(t, ok)
+
+	ok, _ = validatePid(passport{Pid: "000111222"})
+	assert.True(t, ok)
+	ok, _ = validatePid(passport{Pid: "123"})
+	assert.False(t, ok)
+
 }
