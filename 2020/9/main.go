ad123cddcdb29333071198d979275c94d6740452
Kyle Anderson <kyle@xkyle.com>
2020-12-29 22:36:32 -0800

    2020 day 9 part two

--- 2020/9/main.go
+++ 2020/9/main.go
@@ -34,10 +34,39 @@ func partOne(ints []int) int {
 	return findFirstInvalidNumber(ints, 25)
 }
 
+func rangeSumsTo(n int, r []int) bool {
+	sum := 0
+	for i := range r {
+		sum += r[i]
+	}
+	return sum == n
+}
+
+func findContinguousBlockThatAddsTo(n int, ints []int) []int {
+	for start := range ints {
+		for end := start; end <= len(ints); end++ {
+			if rangeSumsTo(n, ints[start:end]) {
+				return ints[start:end]
+			}
+		}
+	}
+	return []int{}
+}
+
+func partTwo(ints []int, firstInvalidNumber int) int {
+	block := findContinguousBlockThatAddsTo(firstInvalidNumber, ints)
+	min := utils.MinInt(block...)
+	max := utils.MaxInt(block...)
+	return min + max
+}
+
 func main() {
 	input := utils.ReadInput()
 	ints := utils.ConvertIntoInts(input)
 
 	result := partOne(ints)
 	fmt.Printf("Answer to part one: %d\n", result)
+
+	result = partTwo(ints, result)
+	fmt.Printf("Answer to part two: %d\n", result)
 }
--- 2020/9/main_test.go
+++ 2020/9/main_test.go
@@ -44,3 +44,40 @@ func Test_isValid(t *testing.T) {
 	assert.False(t, isValid(100, preamble))
 	assert.False(t, isValid(50, preamble))
 }
+
+func Test_rangeSumsTo(t *testing.T) {
+	r := []int{15, 25, 47, 40}
+	assert.True(t, rangeSumsTo(127, r))
+}
+
+func Test_partTwo(t *testing.T) {
+	input := `35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`
+	ints := utils.ConvertIntoInts(input)
+	invalid := findFirstInvalidNumber(ints, 5)
+
+	actualBlock := findContinguousBlockThatAddsTo(invalid, ints)
+	expectedBlock := []int{15, 25, 47, 40}
+	assert.Equal(t, expectedBlock, actualBlock)
+
+	actual := partTwo(ints, invalid)
+	assert.Equal(t, 62, actual)
+}
