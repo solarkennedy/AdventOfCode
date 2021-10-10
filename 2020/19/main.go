e118b07a4e86e28fc99938832c13f50fcdc1a7b5
Kyle Anderson <kyle@xkyle.com>
2021-10-09 21:31:29 -0700

    Working 2020 19 part 1

---
+++ 2020/19/main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/solarkennedy/AdventOfCode/utils"
+)
+
+type rule struct {
+	number   int
+	mainRule string
+	ruleSet1 []int
+	ruleSet2 []int
+	rawRule  string
+}
+
+func getMainRule(input string) string {
+	if strings.Contains(input, `"`) {
+		return strings.Trim(input, `"`)
+	}
+	return ""
+}
+
+func getRuleSet1(input string) []int {
+	r1 := []int{}
+	if !strings.Contains(input, `"`) {
+		lhs := strings.Split(input, `|`)
+		els := strings.Split(strings.TrimSpace(lhs[0]), ` `)
+		return spaceSepratedStringsToArray(els)
+	}
+	return r1
+}
+
+func getRuleSet2(input string) []int {
+	r2 := []int{}
+	if strings.Contains(input, `|`) {
+		lhs := strings.Split(input, `|`)
+		els := strings.Split(strings.TrimSpace(lhs[1]), ` `)
+		r2 = spaceSepratedStringsToArray(els)
+	}
+	return r2
+}
+
+func spaceSepratedStringsToArray(els []string) []int {
+	r := []int{}
+	for _, e := range els {
+		a, _ := strconv.Atoi(e)
+		r = append(r, a)
+	}
+	return r
+}
+
+func parseRule(input string) rule {
+	s := strings.Split(input, `: `)
+	ruleNumber, err := strconv.Atoi(strings.TrimSpace(s[0]))
+	if err != nil {
+		panic(fmt.Sprintf("Error parsing rule '%s'", input))
+	}
+	r := s[1]
+	return rule{
+		number:   ruleNumber,
+		mainRule: getMainRule(r),
+		ruleSet1: getRuleSet1(r),
+		ruleSet2: getRuleSet2(r),
+		rawRule:  strings.TrimSpace(input),
+	}
+}
+
+func parseRules(input string) []rule {
+	splitStrings := strings.Split(strings.TrimSpace(input), "\n")
+	rules := make([]rule, 1000)
+	for _, r := range splitStrings {
+		parsedRule := parseRule(r)
+		rules[parsedRule.number] = parsedRule
+	}
+	return rules
+}
+
+func parseMessages(input string) []string {
+	messages := []string{}
+	for _, s := range strings.Split(strings.TrimSpace(input), "\n") {
+		messages = append(messages, strings.TrimSpace(s))
+	}
+	return messages
+}
+
+func parseMessagesAndRules(input string) ([]rule, []string) {
+	s := strings.Split(input, "\n\n")
+	rules := parseRules(s[0])
+	messages := parseMessages(s[1])
+	return rules, messages
+}
+
+func messageMatchesMainRule(m string, mainRule string) int {
+	if m[0] == byte(mainRule[0]) {
+		return 1
+	}
+	return 0
+}
+
+func messageMatchesMultiRule(message string, rules []rule, ruleNumbers []int) int {
+	counter := 0
+	for _, ri := range ruleNumbers {
+		remaining := message[counter:]
+		if remaining == "" {
+			panic("Wtf")
+		}
+		matched := messageMatchesRule(remaining, rules, ri)
+		if matched == 0 {
+			return 0
+		}
+		counter += matched
+	}
+	return counter
+}
+
+func messageMatchesRule(message string, rules []rule, ruleNumber int) int {
+	r := rules[ruleNumber]
+	if r.mainRule != "" {
+		return messageMatchesMainRule(message, r.mainRule)
+	}
+	if len(r.ruleSet1) != 0 {
+		matched := messageMatchesMultiRule(message, rules, r.ruleSet1)
+		if matched != 0 {
+			return matched
+		}
+	}
+	if len(r.ruleSet2) != 0 {
+		matched := messageMatchesMultiRule(message, rules, r.ruleSet2)
+		if matched != 0 {
+			return matched
+		}
+	}
+	return 0
+}
+
+func messageMatchesRule0(message string, rules []rule) bool {
+	matched := messageMatchesRule(message, rules, 0)
+	return matched == len(message)
+}
+
+func howManyMessagesMatchRule0(rules []rule, messages []string) int {
+	count := 0
+	for _, message := range messages {
+		passed := ""
+		if messageMatchesRule0(message, rules) {
+			count++
+			passed = "passed"
+		}
+		fmt.Printf("message %d: %s %s\n", count, message, passed)
+	}
+	return count
+}
+
+func partOne(input string) int {
+	rules, messages := parseMessagesAndRules(input)
+	return howManyMessagesMatchRule0(rules, messages)
+}
+
+func partTwo(input string) int {
+	rules, messages := parseMessagesAndRules(input)
+	rules[8] = rule{
+		ruleSet1: []int{42},
+		ruleSet2: []int{42, 8},
+	}
+	rules[11] = rule{
+		ruleSet1: []int{42, 31},
+		ruleSet2: []int{42, 11, 43},
+	}
+	return howManyMessagesMatchRule0(rules, messages)
+}
+
+func main() {
+	input := utils.ReadInput()
+	result := partOne(input)
+	fmt.Printf("Answer to part one: %d\n", result)
+
+	result2 := partTwo(input)
+	fmt.Printf("Answer to part two: %d\n", result2)
+}
---
+++ 2020/19/main_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_partOne(t *testing.T) {
+	assert.Equal(t, 2, partOne(`0: 4 1 5
+	1: 2 3 | 3 2
+	2: 4 4 | 5 5
+	3: 4 5 | 5 4
+	4: "a"
+	5: "b"
+
+	ababbb
+	bababa
+	abbbab
+	aaabbb
+	aaaabbb`))
+}
+
+func Test_partTwo(t *testing.T) {
+	input := `42: 9 14 | 10 1
+	9: 14 27 | 1 26
+	10: 23 14 | 28 1
+	1: "a"
+	11: 42 31
+	5: 1 14 | 15 1
+	19: 14 1 | 14 14
+	12: 24 14 | 19 1
+	16: 15 1 | 14 14
+	31: 14 17 | 1 13
+	6: 14 14 | 1 14
+	2: 1 24 | 14 4
+	0: 8 11
+	13: 14 3 | 1 12
+	15: 1 | 14
+	17: 14 2 | 1 7
+	23: 25 1 | 22 14
+	28: 16 1
+	4: 1 1
+	20: 14 14 | 1 15
+	3: 5 14 | 16 1
+	27: 1 6 | 14 18
+	14: "b"
+	21: 14 1 | 1 14
+	25: 1 1 | 1 14
+	22: 14 14
+	8: 42
+	26: 14 22 | 1 20
+	18: 15 15
+	7: 14 5 | 1 21
+	24: 14 1
+
+	abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+	bbabbbbaabaabba
+	babbbbaabbbbbabbbbbbaabaaabaaa
+	aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+	bbbbbbbaaaabbbbaaabbabaaa
+	bbbababbbbaaaaaaaabbababaaababaabab
+	ababaaaaaabaaab
+	ababaaaaabbbaba
+	baabbaaaabbaaaababbaababb
+	abbbbabbbbaaaababbbbbbaaaababb
+	aaaaabbaabaaaaababaa
+	aaaabbaaaabbaaa
+	aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+	babaaabbbaaabaababbaabababaaab
+	aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba`
+	assert.Equal(t, 12, partTwo(input))
+}
+
+func Test_messageMatchesRule0(t *testing.T) {
+	r := parseRules(`0: 4 1 5
+	1: 2 3 | 3 2
+	2: 4 4 | 5 5
+	3: 4 5 | 5 4
+	4: "a"
+	5: "b"`)
+	assert.True(t, messageMatchesRule0(`ababbb`, r))
+	assert.True(t, messageMatchesRule0(`abbbab`, r))
+	assert.False(t, messageMatchesRule0(`bababa`, r))
+	assert.False(t, messageMatchesRule0(`aaabbb`, r))
+	assert.False(t, messageMatchesRule0(`aaaabbb`, r))
+}
+
+func Test_parseRule(t *testing.T) {
+	r := parseRule("0: 4 1 5")
+	assert.Equal(t, []int{4, 1, 5}, r.ruleSet1)
+	r = parseRule("1: 2 3 | 3 2")
+	assert.Equal(t, []int{2, 3}, r.ruleSet1)
+	assert.Equal(t, []int{3, 2}, r.ruleSet2)
+	r = parseRule(`5: "b"`)
+	assert.Equal(t, "b", r.mainRule)
+}
+
+func Test_parseRulesMixedInputs(t *testing.T) {
+	input := `0: 2 3 | 3 2
+	4: 4 4 | 5 5
+	3: 4 5 | 5 4
+	2: "a"
+	1: "b"`
+	actualRules := parseRules(input)
+	assert.Equal(t, 1, actualRules[1].number)
+}
