822d52cc9c9b93852c7c391584c85ed20c854802
Kyle Anderson <kyle@xkyle.com>
2021-10-12 15:55:16 -0700

    2020 day 10 part 1

--- 2020/10/main.go
+++ 2020/10/main.go
@@ -2,13 +2,133 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/solarkennedy/AdventOfCode/utils"
 )
 
+type Adapter struct {
+	//	index  int
+	rating int
+}
+
+type AdapterChain struct {
+	adapters []Adapter
+	unused   []Adapter
+}
+
+func parseAdapters(input string) []Adapter {
+	ints := []int{}
+	for _, s := range strings.Split(strings.TrimSpace(input), "\n") {
+		ints = append(ints, utils.Atoi(s))
+	}
+	adapters := []Adapter{}
+	sort.Ints(ints)
+	for _, i := range ints {
+		adapters = append(adapters, Adapter{rating: i})
+	}
+	return adapters
+}
+
+func (a Adapter) IsCompatibleWithInputVoltage(voltage int) bool {
+	// adapters can only connect to a source 1-3 jolts lower than its rating,
+	return a.rating-voltage >= 1 && a.rating-voltage <= 3
+}
+
+func findSuitableAdapters(adapters []Adapter, voltage int) []int {
+	ret := []int{}
+	for i, a := range adapters {
+		if a.IsCompatibleWithInputVoltage(voltage) {
+			ret = append(ret, i)
+			// Hack for speed, always the first one we find???
+			return ret
+		}
+	}
+	return ret
+}
+
+func getLastVoltage(adapters []Adapter) int {
+	if len(adapters) == 0 {
+		// The charging outlet has an effective rating of 0 jolts
+		return 0
+	}
+	return adapters[len(adapters)-1].rating
+}
+
+func findWorkingChains(c AdapterChain) []AdapterChain {
+	if len(c.unused) == 0 {
+		return []AdapterChain{c}
+	}
+	fmt.Printf("%sDepth %d\n", strings.Repeat(" ", len(c.adapters)), len(c.adapters))
+	workingChains := []AdapterChain{}
+	voltage := getLastVoltage(c.adapters)
+	suitableAdapters := findSuitableAdapters(c.unused, voltage)
+	for _, i := range suitableAdapters {
+		suitableAdapter := c.unused[i]
+		fmt.Printf("Going to try adding %+v to a chain of %+v\n", suitableAdapter, c)
+		newUnused := removeAdapter(c.unused, i)
+		newAdapters := append(c.adapters, suitableAdapter)
+		newChain := AdapterChain{
+			adapters: newAdapters,
+			unused:   newUnused,
+		}
+		// fmt.Printf("   New chain: %+v\n", newChain)
+		newChains := findWorkingChains(newChain)
+		workingChains = append(workingChains, newChains...)
+	}
+	// if len(workingChains) == 0 {
+	// 	fmt.Printf("%sNo valid chains found after %+v\n", strings.Repeat(" ", len(c.adapters)), c)
+	// }
+	return workingChains
+}
+
+func removeAdapter(s []Adapter, index int) []Adapter {
+	ret := make([]Adapter, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
+func findWorkingChain(adapters []Adapter) AdapterChain {
+	fmt.Printf("Building chains using %d different adapters...\n%+v\n", len(adapters), adapters)
+	chains := findWorkingChains(AdapterChain{unused: adapters})
+	if len(chains) != 1 {
+		// Shouldn't happen
+		return AdapterChain{}
+	}
+	fmt.Printf("FOUND IT: %+v\n", chains[0])
+	return chains[0]
+}
+
+func countJoltDifferences(adapters []Adapter, diff int) int {
+	total := 0
+	for i := range adapters {
+		if i == len(adapters)-1 {
+			continue
+		}
+		differential := adapters[i+1].rating - adapters[i].rating
+		if differential == diff {
+			total++
+		}
+	}
+	// Last built-in adapter is always 3 higher than the first
+	if diff == 3 {
+		total++
+	}
+	// Wall has 0
+	wallDiff := adapters[0].rating - 0
+	if wallDiff == diff {
+		total++
+	}
+	return total
+}
+
 func partOne(input string) int {
-	return 42
+	adapters := parseAdapters(input)
+	chain := findWorkingChain(adapters)
+	ones := countJoltDifferences(chain.adapters, 1)
+	threes := countJoltDifferences(chain.adapters, 3)
+	return ones * threes
 }
 
 func partTwo(input string) int {
--- 2020/10/main_test.go
+++ 2020/10/main_test.go
@@ -7,10 +7,79 @@ import (
 )
 
 func Test_partOne(t *testing.T) {
-	assert.Equal(t, 42, partOne("1,3,2"))
+	input := `
+16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`
+	assert.Equal(t, 35, partOne(input))
+}
+
+func Test_isCompatibleWithInputVoltage(t *testing.T) {
+	a := Adapter{rating: 1}
+	assert.True(t, a.IsCompatibleWithInputVoltage(0))
+	a = Adapter{rating: 4}
+	assert.False(t, a.IsCompatibleWithInputVoltage(0))
+
+	a = Adapter{rating: 4}
+	assert.False(t, a.IsCompatibleWithInputVoltage(4))
+	a = Adapter{rating: 5}
+	assert.True(t, a.IsCompatibleWithInputVoltage(4))
+	a = Adapter{rating: 6}
+	assert.True(t, a.IsCompatibleWithInputVoltage(4))
+	a = Adapter{rating: 7}
+	assert.True(t, a.IsCompatibleWithInputVoltage(4))
+	a = Adapter{rating: 8}
+	assert.False(t, a.IsCompatibleWithInputVoltage(4))
+
+	a = Adapter{rating: 6}
+	assert.True(t, a.IsCompatibleWithInputVoltage(5))
+}
+
+func Test_removeAdapter(t *testing.T) {
+	a := parseAdapters("1\n2\n3")
+	assert.Equal(t, 3, len(a))
+	b := removeAdapter(a, 0)
+	assert.Equal(t, 3, len(a))
+	assert.Equal(t, 2, len(b))
+	c := removeAdapter(b, 0)
+	assert.Equal(t, 3, len(a))
+	assert.Equal(t, 2, len(b))
+	assert.Equal(t, 1, len(c))
+	d := removeAdapter(c, 0)
+	assert.Equal(t, 3, len(a))
+	assert.Equal(t, 2, len(b))
+	assert.Equal(t, 1, len(c))
+	assert.Equal(t, 0, len(d))
+}
+
+func Test_removeAdapterObject(t *testing.T) {
+	a := parseAdapters("1\n2\n3")
+	assert.Equal(t, 3, len(a))
+	ch := AdapterChain{unused: a}
+	b := removeAdapter(ch.unused, 0)
+	assert.Equal(t, 3, len(ch.unused))
+	assert.Equal(t, 1, ch.unused[0].rating)
+	assert.Equal(t, 2, len(b))
+	assert.Equal(t, 2, b[0].rating)
+
+}
+
+func Test_countJoltDifferences(t *testing.T) {
+	a := parseAdapters("1\n4\n5\n6\n7\n10\n11\n12\n15\n16\n19")
+	ones := countJoltDifferences(a, 1)
+	assert.Equal(t, 7, ones)
+	threes := countJoltDifferences(a, 3)
+	assert.Equal(t, 5, threes)
 }
 
 func Test_partTwo(t *testing.T) {
 	assert.Equal(t, 43, partTwo("1,2,3"))
 }
-
