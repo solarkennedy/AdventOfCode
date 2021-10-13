9be3e9535d9623a9352fb883d0d44dbb5f268eb8
Kyle Anderson <kyle@xkyle.com>
2021-10-12 17:33:56 -0700

    2020 day 14 part 1

--- 2020/14/main.go
+++ 2020/14/main.go
@@ -7,8 +7,82 @@ import (
 	"github.com/solarkennedy/AdventOfCode/utils"
 )
 
+type instruction struct {
+	mask         string
+	location     int
+	preMaskValue int
+}
+
+func parseMemLocation(input string) int {
+	first := strings.Split(input, "[")[1]
+	second := strings.Split(first, "]")[0]
+	return utils.Atoi(second)
+
+}
+
+func parseInstructions(input string) []instruction {
+	ins := []instruction{}
+	var mask string
+	for _, s := range strings.Split(strings.TrimSpace(input), "\n") {
+		tokens := strings.Split(s, " = ")
+		if tokens[0] == "mask" {
+			mask = tokens[1]
+		} else {
+			ins = append(ins, instruction{
+				mask:         mask,
+				location:     parseMemLocation(tokens[0]),
+				preMaskValue: utils.Atoi(tokens[1]),
+			})
+		}
+	}
+	return ins
+}
+
+func bitSet(value int, place int) int {
+	realMask := 1 << place
+	return value | realMask
+}
+
+func bitClear(value int, place int) int {
+	realMask := 1 << place
+	return value &^ realMask
+}
+
+func computePostMaskValue(preValue int, mask string) int {
+	value := preValue
+	fmt.Printf("Value: %036b (%d)\n", value, value)
+	fmt.Printf("Mask : %s\n", mask)
+	for c := range mask {
+		if mask[c] == 'X' {
+			continue
+		} else if mask[c] == '1' {
+			value = bitSet(value, 35-c)
+		} else if mask[c] == '0' {
+			value = bitClear(value, 35-c)
+		} else {
+			panic(mask[c])
+		}
+	}
+	fmt.Printf("Res  : %036b (%d)\n", value, value)
+	return value
+}
+
+func computePostMaskValues(ins []instruction) int {
+	m := map[int]int{}
+	for i := range ins {
+		fmt.Printf("Ins %d (%d):\n", i, ins[i].location)
+		m[ins[i].location] = computePostMaskValue(ins[i].preMaskValue, ins[i].mask)
+	}
+	total := 0
+	for _, v := range m {
+		total += v
+	}
+	return total
+}
+
 func partOne(input string) int {
-	return 42
+	ins := parseInstructions(input)
+	return computePostMaskValues(ins)
 }
 
 func partTwo(input string) int {
--- 2020/14/main_test.go
+++ 2020/14/main_test.go
@@ -6,11 +6,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const basicTestInput = `
+mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0`
+
+const part2Input = `
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`
+
 func Test_partOne(t *testing.T) {
-	assert.Equal(t, 42, partOne("1,3,2"))
+	assert.Equal(t, 165, partOne(basicTestInput))
+}
+
+func Test_computePostMaskValue(t *testing.T) {
+	assert.Equal(t, 73, computePostMaskValue(11, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"))
+	assert.Equal(t, 101, computePostMaskValue(101, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"))
+	assert.Equal(t, 64, computePostMaskValue(0, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"))
 }
 
 func Test_partTwo(t *testing.T) {
-	assert.Equal(t, 43, partTwo("1,2,3"))
+	assert.Equal(t, 208, partTwo(part2Input))
 }
-
